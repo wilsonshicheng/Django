1b2fb249bd86540c821cd7d5db8dee519824af77
wilsonshicheng <415116412@qq.com>
2020-05-17 10:50:26 +0800

    查询请求体数据（表单类型/非表单数据【json/xml】)

--- Django_base_t/request_response/urls.py
+++ Django_base_t/request_response/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns = [
     # 查询字符串不是路径，是路径中的参数，所以路由不用匹配字符串
     path('querystring/',views.QSParmView.as_view()),
+    path('formdata/',views.FormDataParamView.as_view()),
+    path('json/',views.JSONParamView.as_view()),
 
 ]
--- Django_base_t/request_response/views.py
+++ Django_base_t/request_response/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 from django import http
+import json
 
 
 # Create your views here.
-
+# ===========================测试查询字符串==========================
 class QSParmView(View):
- # 测试查询字符串
+
 
     def get(self,request):
         # request.GET:专门用于提取请求地址中的查询字符串参数
@@ -27,3 +28,39 @@ class QSParmView(View):
         print(name, age)
 
         return http.HttpResponse('POST测试提取字符串')
+
+# ===========================测试查询请求体==========================
+
+# 1.查询表单数据
+class FormDataParamView(View):
+    # POST http://127.0.0.1/8000/formdata/
+    def post(self,request):
+        # 方式：request.POST.
+        # form_data = request.POST
+        # username = form_data.get('username')
+        # password = form_data.get('password')
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username,password)
+
+        return http.HttpResponse('测试提取表单类型的请求体数据')
+
+
+# 2.查询Jason数据
+# 非表单类型的请求体数据有很多种：jason、xml。。。
+# 由于类型种类很多，Django不想知道当前是哪一种。如果需要区分比较复杂，所以django没有添加解析功能。
+# 解决方案：Django提供了了一个属性，用于接收非表单类型的请求体数据。，提取数据后，自行处理。
+class JSONParamView(View):
+    # POST http://127.0.0.1/8000/jason/
+    def post(self,request):
+
+        # 先获取非表单类型的请求体数据：方式：request.body
+        origin_data = request.body
+        # json的loads方法：讲原始的json数据转成字典
+        json_dict = json.loads(origin_data)
+        username = json_dict.get('username')
+        password = json_dict.get('password')
+        print(username,password)
+
+        return http.HttpResponse('测试提取表单类型的请求体数据:JSON')
