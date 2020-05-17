10dca8838075c60a8960d484556b38e590d71377
wilsonshicheng <415116412@qq.com>
2020-05-17 08:57:01 +0800

    函数视图增加请求方式判断

--- Django_base_t/users/views.py
+++ Django_base_t/users/views.py
@@ -3,4 +3,7 @@ from django import http
 # Create your views here.
 
 def register(request):
-    return http.HttpResponse('这是一个注册页面')
+    if request.method=="GET":
+        return http.HttpResponse('这是一个注册页面')
+    else:
+        return http.HttpResponse('这是一个注册逻辑')
