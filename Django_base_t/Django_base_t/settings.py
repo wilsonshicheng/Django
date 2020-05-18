7d1bb2a45108c127248830b6df864e132d102a01
wilsonshicheng <415116412@qq.com>
2020-05-18 16:36:03 +0800

    模型-定义模型类

--- Django_base_t/Django_base_t/settings.py
+++ Django_base_t/Django_base_t/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'request_response'
+    'request_response',
+    'booktest'
 ]
 
 MIDDLEWARE = [
